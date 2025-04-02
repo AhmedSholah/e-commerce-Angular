e823a7cf0dc00fc62883d76c64c6effe8fb9f7de
Yousef Abdelrhem <73524516+Yousef-Abdelrhem@users.noreply.github.com>
2025-04-02 08:56:55 +0200

    merge with develop

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,36 +1,32 @@
 import { Injectable } from '@angular/core';
-import { API } from './apiConfig';
+import {API} from './apiConfig'
 import { HttpClient } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root'
 })
 export class AuthService {
-    baseUrl = API.baseUrl;
-    constructor(private http: HttpClient) {}
+  baseUrl = API.baseUrl;
+  constructor(private http: HttpClient) {}
+  
+  login(credentials: {email: string , password: string}) : Observable<any>{
+    return this.http.post(`${this.baseUrl}${API.auth.login}`, credentials).pipe(
+      tap((response: any) => {
+          console.log(response.data.token);
+          if(response.data.token)
+          {
+            localStorage.setItem('authToken', response.data.token);
+          }
+      })
+    );
+  }
+  
+  register(credentials: {firstName: string, lastName: string , email: string, gender:string, password:string, role:string}): Observable<any>
+  {
+    return this.http.post(`${this.baseUrl}${API.auth.register}`, credentials);
+  }
 
-    login(credentials: { email: string; password: string }): Observable<any> {
-        return this.http.post(`${this.baseUrl}${API.auth.login}`, credentials).pipe(
-            tap((response: any) => {
-                console.log(response.data.token);
-                if (response.data.token) {
-                    localStorage.setItem('authToken', response.data.token);
-                }
-            })
-        );
-    }
-
-    register(credentials: {
-        username: string;
-        email: string;
-        gender: string;
-        password: string;
-        role: string;
-    }): Observable<any> {
-        return this.http.post(`${this.baseUrl}${API.auth.register}`, credentials);
-    }
-
-    logout() {
-        localStorage.removeItem('authToken');
-    }
+  logout(){
+    localStorage.removeItem('authToken');
+  }
 }
